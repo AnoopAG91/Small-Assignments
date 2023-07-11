b32093fb320a52654bca3d1e531545c5a5dd0f4c
AnoopAG91 <anoopag47@gmail.com>
2023-07-11 22:57:19 +0530

    added imogi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
 import './App.css';
+import EmojiPicker from 'emoji-picker-react';
 
 function App() {
   return (
     <div className="App">
-     <h1>Select Your Imogi</h1>
+      <div className='imogi-wrapper'>
+      <h1>Select Your Imogi</h1>
+     
+      <EmojiPicker/>
+      </div>
+   
     </div>
+
   );
 }
 
